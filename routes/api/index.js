5761d37f33dc53dc601dce066e5a262e9a037ce0
Ben Armstrong <benarmstrong@live.com>
2023-04-25 20:44:05 +0930

    debugged reactions

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,8 @@
 //--------Required express package and paths:
 const router = require('express').Router();
-        // const courseRoutes = require('./courseRoutes');
-        // const studentRoutes = require('./studentRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-        // router.use('/courses', courseRoutes);
-        // router.use('/students', studentRoutes);
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 
